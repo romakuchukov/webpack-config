91a55033f99e496673d156a6e33e7a30dc142fd0
Roma K <romankuchukov@gmail.com>
2019-12-04 13:47:05 -0500

    gutted util plugin

--- src/plugins/util.js
+++ src/plugins/util.js
@@ -1,19 +1,12 @@
 "use strict";
 
 const util = {
-    mo: 500,
-    sm: 650,
-    lx: 1200,
-    get moDn() { return this.mo - .1; },
-    get smDn() { return this.sm - .1; },
-    get lxDn() { return this.lx - .1; }
+
 };
 
-console.log('fsdfsdfsdfsdfsd')
-
-Object.defineProperty(util, 'max', {
-    enumerable: true,
-    get: () => util.lx,
-});
+// Object.defineProperty(util, 'max', {
+//     enumerable: true,
+//     get: () => util.lx,
+// });
 
 export default Object.freeze(util);
